7534278350f622f465f8be02a65cd31730999351
kantizepatrick <kantizepatrick@gmail.com>
2026-06-14 22:17:33 +0200

    Adjust CAS logo size to 220px (larger)

--- src/components/Login.js
+++ src/components/Login.js
@@ -200,7 +200,7 @@ const Login = ({ onLogin }) => {
             src={logo} 
             alt="CAS Ground Services" 
             style={{ 
-              width: '200px',
+              width: '220px',
               height: 'auto',
               maxWidth: '100%',
               marginBottom: '20px'
